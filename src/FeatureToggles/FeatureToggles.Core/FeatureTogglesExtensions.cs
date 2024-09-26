3fea40f323fea1e87730de6762a07258ba3f48bf
Fredrik Nilsson <ftnilsson@gmail.com>
2024-09-26 20:05:19 +0200

    Create FeatureTogglesExtensions.cs
    new extension class

---
+++ src/FeatureToggles/FeatureToggles.Core/FeatureTogglesExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FeatureToggles.Core.Models;
+
+namespace FeatureToggles.Core;
+
+public static class FeatureToggleExtensions
+{
+    public static bool IsActive(this FeatureToggle[] flags, string name)
+    {
+        var flag = flags?.FirstOrDefault(x => x.Name.Equals(name));
+
+        return flag is not null && flag.IsActive;
+    }
+}
